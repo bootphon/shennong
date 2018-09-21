f59f2de80648fc599cd8c5a86d5d3ab7db3bb915
Mathieu Bernard <mathieu_bernard@laposte.net>
2018-09-21 16:00:12 +0200

    improved alignment

--- shennong/alignment.py
+++ shennong/alignment.py
@@ -2,26 +2,24 @@
 
 import gzip
 import os
-import math
 import numpy as np
 
 
 class AlignmentCollection(dict):
     """A dictionary of time alignments indexed by items
 
-    An AlignmentCollection is a usual Python dictionnary with some
-    additional functions. Keys are strings and values are `Alignment`
-    instances. Depending on the underlying data, a key can be a single
-    utterance or the speech corresponding to an entire WAV file or
-    speaker.
+    An AlignmentCollection is a usual Python dictionary with some
+    additional functions. Keys are strings, values are `Alignment`
+    instances. Depending on the underlying data, keys can represents a
+    single utterance, a single speaker or an entire WAV file.
 
     Parameters
     ----------
-    data : sequence of (item, tstart, tstop, phone)
-       A list or a sequence of quadruplets `(item, tstart, tstop,
+    data : sequence of (item, onset, offset, phone)
+       A list or a sequence of quadruplets `(item, onset, offset,
        phone)` representing a time aligned phone for a given
-       `item`. `tstart` is the start timestamp of the pronunced phone,
-       `tstop` is the end timestamp of the pronunciation and `phone`
+       `item`. `onset` is the start timestamp of the pronunced phone,
+       `offset` is the end timestamp of the pronunciation and `phone`
        is a string representation of the phone. `tstart` and `tstop`
        are expressed in seconds.
 
@@ -33,8 +31,13 @@ class AlignmentCollection(dict):
     Raises
     ------
     ValueError
-        If one element of `data` is not a quadruplet, or if one
-        `tstart` or `tstop` cannot be casted to float
+        If one element of `data` is not a quadruplet, if one `tstart`
+        or `tstop` cannot be casted to float
+
+    See Also
+    --------
+    Alignment
+        The values of the AlignmentCollection dictionary
 
     """
     def __init__(self, data):
@@ -56,7 +59,8 @@ class AlignmentCollection(dict):
         # second init: from list to Alignment
         for item, data in self.items():
             try:
-                self[item] = Alignment(data, phones_set=self.phones_set)
+                self[item] = Alignment.from_list(
+                    data, phones_set=self.phones_set, validate=True)
             except ValueError as err:
                 raise ValueError('item {}: {}'.format(item, err))
 
@@ -66,7 +70,7 @@ class AlignmentCollection(dict):
         The saved alignment is in text format (encoded in utf8), each
         line structured as::
 
-            <item> <tstart> <tstop> <phone>
+            <item> <onset> <offset> <phone>
 
         Parameters
         ----------
@@ -109,7 +113,8 @@ class AlignmentCollection(dict):
 
     def _list_str(self, item):
         """Returns an alignment item as a list of strings"""
-        return [item + ' ' + line for line in self[item]._list_str()]
+        return [item + ' ' + '{} {} {}'.format(l[0], l[1], l[2])
+                for l in self[item].to_list()]
 
     @staticmethod
     def load(alignment_file, compress=False):
@@ -119,7 +124,7 @@ class AlignmentCollection(dict):
         utf8. It must be composed of lines with the following 4
         fields:
 
-            <item> <tstart> <tstop> <phone>
+            <item> <onset> <offset> <phone>
 
         Parameters
         ----------
@@ -156,91 +161,141 @@ class Alignment:
     """Time alignment of phones
 
     An Alignment handles a time alignment of phones, i.e. a suite of
-    phones linked with their onset (tstart) and offset (tstop)
-    timestamps.
-
-    The timestamps in `data` must fill the following conditions to
-    ensure temporal consistency:
-
-    * `tstart` must be sorted in increasing order: `data` is a
-       temporal sequence
-
-    * `tstart` must be lesser than `tstop`: each phone in `data` has a
-      positive duration
-
-    * `tstop[n]` must be equal to `tstart[n+1]`: `data` has a temporal
-      continuity.
-
-    Parameters
-    ----------
-    data : sequence of (tstart, tstop, phone)
-        A list or sequence of triplets `(tstart, tstop, phone)`
-        representing a time aligned phone. `tstart` is the start
-        timestamp of the pronunced phone, `tstop` is the end timestamp
-        of the pronunciation and `phone` is a string representation of
-        the phone. `tstart` and `tstop` are expressed in seconds.
-    phones_set : set, optional
-        The set of the different phones present in the alignment. If
-        not specified the `phones_set` is built from the phones in
-        `data`.
+    phones linked with their onset and offset timestamps. See the
+    `validate` method for a list constraints applying to the `data`.
 
     Attributes
     ----------
-    times : array of float32, shape = [nphones, 2]
-        The array of (tstart, tstop) timestamps for each aligned phone
+    times : array of float, shape = [nphones, 2]
+        The array of (onset, offset) timestamps for each aligned phone
     phones : array of str, shape = [nphones, 1]
-       The array of aligned phones
+        The array of aligned phones
     phones_set : set
-       The set of the distinct phones present in the alignement
+        The set of the distinct phones present in the alignement
+    validate : bool, optional
+        When True, checks the alignment is in a valid format, when
+        False does not perform any verification, default is True
 
     Raises
     ------
     ValueError
-        If the timestamps in `data` are not temporally
-        consistent. When `phones_set` is defined and a phone of the
-        alignment is not deefiened in the set.
+        When `validate` is True and the alignment data is not
+        correctly formatted
 
     """
-    def __init__(self, data, phones_set=None):
-        # load the timestamps as (tstart, tstop)
-        self.times = np.array([entry[:2] for entry in data], dtype=np.float32)
-
-        # check tstart < tstop for all timestamps
-        for i in range(self.times.shape[0]):
-            if self.times[i, 0] >= self.times[i, 1]:
-                raise ValueError(
-                    'tstart must be lesser than tstop')
-
-        # check tstarts are sorted in increasing order and tstop[n]
-        # matches tstart[n+1]
-        for i in range(self.times.shape[0] - 1):
-            if self.times[i, 0] > self.times[i+1, 0]:
-                raise ValueError(
-                    'timestamps must be sorted in increasing order')
-            if self.times[i, 1] != self.times[i+1, 0]:
-                raise ValueError(
-                    'mismatch in tstop/tstart timestamps')
-
-        self.phones = np.array([d[2] for d in data])
+    def __init__(self, times, phones, phones_set=None, validate=True):
+        self._times = times
+        self.phones = phones
 
         # if the phone set is unspecified, take it from the alignment
         if phones_set is None:
             self.phones_set = set(self.phones)
         else:
-            # phones_set is specified, make sure all phones in the
-            # alignment are present in the phones_set
-            phone_diff = set(self.phones).difference(set(phones_set))
-            if len(phone_diff) != 0:
-                raise ValueError(
-                  'following phones are in alignment but not in phones_set: '
-                  '{}'.format(', '.join(phone_diff)))
-
             self.phones_set = phones_set
 
+        if validate is True:
+            self.validate()
+
+    @property
+    def onsets(self):
+        """The start timestamps of the aligned phones in seconds"""
+        return self._times[:, 0]
+
+    @property
+    def offsets(self):
+        """The stop timestamps of the aligned phones in seconds"""
+        return self._times[:, 1]
+
+    @staticmethod
+    def from_list(data, phones_set=None, validate=True):
+        """Build an Alignment from a list of (tstart, tsop, phone) triplets
+
+        This method checks all elements in the `data` list have 3
+        fields, convert them to `times` and `data` arrays, and
+        instanciates an Alignment instance with them.
+
+        Parameters
+        ----------
+        data : sequence of (tstart, tstop, phone)
+            A list or sequence of triplets `(tstart, tstop, phone)`
+            representing a time aligned phone. `tstart` and `tstop`
+            are the onset and offset of the pronunciation (in
+            seconds). `phone` is a string representation of the phone.
+        phones_set : set,
+            optional The set of the different phones present in the
+            alignment. If not specified the `phones_set` is built from
+            the phones in `data`.
+
+        """
+        # check we have 3 fields in each data entry
+        for i, entry in enumerate(data):
+            if len(entry) != 3:
+                raise ValueError(
+                    'line {}: entry must have 3 fields but has {}'
+                    .format(i, len(entry)))
+
+        times = np.array([d[:2] for d in data], dtype=np.float)
+        phones = np.array([d[2] for d in data])
+        return Alignment(
+            times, phones, phones_set=phones_set, validate=validate)
+
+    def validate(self):
+        """Raises a ValueError is the Alignment is not consistent
+
+        The following conditions must apply for the alignment to be
+        valid:
+
+        * `onsets`, `offsets` and `phones` must have the same length
+
+        * `onsets` and `offsets` must be sorted in increasing order:
+           `data` is a temporal sequence
+
+        * `onsets[n]` must be lesser than `offsets[n]`: each phone in
+          `data` has a strictly positive duration
+
+        * `offsets[n]` must be equal to `onsets[n+1]`: `data` has a
+          temporal continuity.
+
+        """
+        # same length for timestamps and phones
+        if not self._times.shape[0] == self.phones.shape[0]:
+            raise ValueError('timestamps and phones must have the same length')
+
+        # check tstart < tstop for all timestamps
+        for i in range(self.phones.shape[0]):
+            if self.onsets[i] >= self.offsets[i]:
+                raise ValueError(
+                    'phone {}: onset must be lesser than offset'.format(i))
+
+        # check tstarts are sorted in increasing order and tstop[n]
+        # matches tstart[n+1]
+        for i in range(self.phones.shape[0] - 1):
+            if self.onsets[i] > self.onsets[i+1]:
+                raise ValueError(
+                    'timestamps must be sorted in increasing order')
+            if self.offsets[i] != self.onsets[i+1]:
+                raise ValueError(
+                    'mismatch in tstop/tstart timestamps')
+
+        # make sure all phones in alignment are present in phones_set
+        diff = set(self.phones).difference(set(self.phones_set))
+        if len(diff) != 0:
+            raise ValueError(
+                'following phones are in alignment but not in phones_set: '
+                '{}'.format(', '.join(diff)))
+
+    def is_valid(self):
+        """Returns True if the Alignment is consistent, False otherwise"""
+        try:
+            self.validate()
+        except ValueError:
+            return False
+        return True
+
     def __eq__(self, other):
-        return (np.array_equal(self.times, other.times)
+        return (np.array_equal(self._times, other._times)
                 and np.array_equal(self.phones, other.phones)
-                and self.phones_set == self.phones_set)
+                and self.phones_set == other.phones_set)
 
     def __getitem__(self, time):
         """Returns data aligned in `time` slice
@@ -252,60 +307,122 @@ class Alignment:
 
         Parameters
         ----------
-        time : slice (start, stop), in seconds
+        time : slice (onset, offset), in seconds
             The time interval on which to extract the alignment is
             defined by `slice.start` and `slice.stop`, expressed in
             seconds. `slice.step` is not used.
 
         Returns
         -------
-        times : array of float32
-            The timestamps within the given `time` interval
-        phones : array of string
-            The phones within the given `time` interval
+        alignment : Alignment
+            The sub-alignment rextracted from the original one
 
         Raises
         ------
         ValueError
-            If `time` is not a slice
+            If `time` is not a slice, or if `time.step` is defined.
 
         """
         if not isinstance(time, slice):
             raise ValueError(
                 'time must be a slice but is {}'.format(type(time)))
 
-        # setup the start and stop timestamp from the `time` slice
-        tstart = self.times[0, 0] if time.start is None else time.start
-        tstop = self.times[-1, 1] if time.stop is None else time.stop
+        if time.step is not None:
+            raise ValueError('time.step is defined but is useless')
 
-        # better to use np.searchsorted than np.where here,
-        # considering the times are sorted
-        istart = np.searchsorted(self.times[:, 0], tstart, side='left')
-        istop = np.searchsorted(self.times[:, 1], tstop, side='right')
-        return self.times[istart:istop], self.phones[istart:istop]
+        # setup the start and stop timestamp from the `time` slice,
+        # bound them at start and stop timestamps of the alignment
+        tmin = self.onsets[0]
+        tstart = time.start
+        if tstart is None or tstart < tmin:
+            tstart = tmin
 
-    def _list_str(self):
-        """Returns the alignement as a list of strings"""
-        return ['{:.5f} {:.5f} {}'.format(  # float precision to 5 decimals
-            self.times[i, 0], self.times[i, 1], self.phones[i])
-                for i in range(self.times.shape[0])]
+        tmax = self.offsets[-1]
+        tstop = time.stop
+        if tstop is None or tstop > tmax:
+            tstop = tmax
 
-    def at_sample_rate(self, sample_rate, tstart=None, tstop=None):
-        """Yields phones read at the given `sample_rate`"""
-        # the phones to read
-        times, phones = self[tstart:tstop]
+        # deal with corner cases
+        if tstart >= tstop or tstart >= tmax or tstop <= tmin:
+            return Alignment(
+                np.array([]), np.array([]),
+                phones_set=self.phones_set,
+                validate=False)
 
-        for i, (tstart, tstop) in enumerate(times):
-            # number of times the phone is read at the given
-            # samplerate
-            nread = math.floor((tstop - tstart) * sample_rate)
-            for _ in range(nread):
-                yield phones[i]
+        if tstart == tmin and tstop == tmax:
+            return self
+
+        # now (tstart, tstop) are in boundaries
+        assert tmin <= tstart < tstop <= tmax
+
+        # TODO in the following lines we can optimize. This is useless
+        # to do a np.where on the whole timestamps, can we restrict
+        # the area of search using np.searchsorted for instance?
+
+        # find the start index (last <= tstart)
+        if tstart == tmin:
+            istart = 0
+        else:
+            istart = np.where(self.onsets <= tstart)[0][-1]
+
+        # find the stop index (first >= tstop)
+        if tstop == tmax:
+            istop = self.phones.shape[0] - 1
+        else:
+            istop = np.where(self.offsets >= tstop)[0][0]
+
+        # we have a partial read of a single phone
+        if istart == istop:
+            phones = np.array(self.phones[istart:istart+1])
+            times = np.array([tstart, tstop]).reshape(1, 2)
+        else:  # build the computed subalignment
+            phones = self.phones[istart:istop+1]
+            times = np.copy(self._times[istart:istop+1, :])
+            times[0, 0] = tstart
+            times[-1, 1] = tstop
+
+        return Alignment(
+            times, phones,
+            phones_set=self.phones_set,
+            validate=False)
+
+    def __repr__(self):
+        return '\n'.join(
+            '{} {} {}'.format(t0, t1, p)
+            for (t0, t1, p) in self.to_list())
+
+    def to_list(self):
+        """Returns the alignment as a list of triplets (onset, offset, phone)
+
+        See Also
+        --------
+        Alignment.from_list
+            The reverse operation
+
+        """
+        return [(self.onsets[i], self.offsets[i], self.phones[i])
+                for i in range(self.phones.shape[0])]
+
+    def at_sample_rate(self, sample_rate):
+        """Returns an array of phones read at the given `sample_rate`"""
+        # allocate the result data
+        data = np.zeros(
+            (int(self.duration() * sample_rate),),
+            dtype=self.phones.dtype)
+
+        # the sampled timestamps
+        times = np.arange(data.shape[0]) / sample_rate + self.onsets[0]
+
+        j = 0
+        for i in range(data.shape[0]):
+            while times[i] >= self.offsets[j]:
+                j += 1
+            data[i] = self.phones[j]
+
+        return data
 
     def duration(self):
-        """Returns the duration of the aligned speech, in seconds"""
-        return self.times[-1, 1] - self.times[0, 0]
-
-    def nphones(self):
-        """Returns the number of distinct phones in the alignment"""
-        return len(self.phones_set)
+        """Returns the duration of the alignment in seconds"""
+        if len(self.phones) == 0:
+            return 0
+        return self.offsets[-1] - self.onsets[0]
--- test/test_alignment.py
+++ test/test_alignment.py
@@ -6,77 +6,213 @@ import pytest
 from shennong.alignment import Alignment, AlignmentCollection
 
 
-def test_simple():
-    a = [(0, 1, 'a'), (1, 2, 'b'), (2, 3.001, 'c')]
-    ali = Alignment(a)
+@pytest.fixture(scope='session')
+def ali():
+    return Alignment.from_list([(0, 1, 'a'), (1, 2, 'b'), (2, 3.001, 'c')])
+
+
+def test_simple(ali):
     assert ali.phones.shape == (3,)
-    assert ali.times.shape == (3, 2)
-    assert ali.times.dtype == np.float32
+    assert ali._times.shape == (3, 2)
+    assert ali._times.dtype == np.float
     assert ali.duration() == pytest.approx(3.001)
 
     assert ali.phones_set == {'a', 'b', 'c'}
     assert np.array_equal(np.array(['a', 'b', 'c']), ali.phones)
-    assert np.array([[0, 1], [1, 2], [2, 3.001]]) == pytest.approx(ali.times)
+    assert np.array([[0, 1], [1, 2], [2, 3.001]]) == pytest.approx(ali._times)
 
-    times, phones = ali[0:1]
-    assert np.array_equal(np.array([[0, 1]]), times)
-    assert np.array_equal(np.array(['a']), phones)
+    with pytest.raises(ValueError):
+        ali[1]
 
-    times, phones = ali[0:2]
-    assert np.array_equal(np.array([[0, 1], [1, 2]]), times)
-    assert np.array_equal(np.array(['a', 'b']), phones)
+    with pytest.raises(ValueError):
+        ali[1:2:0]
 
-    times, phones = ali[:2]
-    assert np.array_equal(np.array([[0, 1], [1, 2]]), times)
-    assert np.array_equal(np.array(['a', 'b']), phones)
+    with pytest.raises(ValueError):
+        ali[::0]
 
-    times, phones = ali[0:]
-    assert np.array_equal(ali.times, times)
-    assert np.array_equal(ali.phones, phones)
 
-    times, phones = ali[-5:5]
-    assert np.array_equal(ali.times, times)
-    assert np.array_equal(ali.phones, phones)
+def test_valid(ali):
+    assert ali.is_valid()
 
-    times, phones = ali[:]
-    assert np.array_equal(ali.times, times)
-    assert np.array_equal(ali.phones, phones)
+    ali = Alignment.from_list([])
+    assert ali.is_valid()
 
-    times, phones = ali[:-1]
-    assert len(times) == 0
-    assert len(phones) == 0
+    ali = Alignment.from_list([(0, 0, 'a')], validate=False)
+    assert not ali.is_valid()
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([(0, 1, 'a'), (1, 2, 'b'), (0, 3, 'a')])
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([(0, 1, 'a'), (0, 3, 'c')])
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([(0, 0, 'a')])
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([(1, 0, 'a')])
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([('a', 0, 'a')])
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([(0, 'a', 'a')])
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([(0, 1)])
+
+    with pytest.raises(ValueError):
+        Alignment.from_list([(0, 1, 'a', 'a')])
+
+
+def test_list(ali):
+    ali2 = Alignment(
+        np.array([[0, 1], [1, 2]]), np.array(['a', 'b']),
+        phones_set=ali.phones_set)
+
+    assert ali[:2] == ali2
+    assert ali2.to_list() == [(0, 1, 'a'), (1, 2, 'b')]
+    assert Alignment.from_list(ali.to_list()) == ali
+
+    ali3 = Alignment(
+        np.array([[0, 1], [1, 2]]), np.array(['a', 'b']))
+
+    assert ali2.phones_set != ali3.phones_set
+    assert ali2 != ali3
+    assert ali2.to_list() == [(0, 1, 'a'), (1, 2, 'b')]
+
+
+def test_repr(ali):
+    assert str(ali[:1]) == '0.0 1.0 a'
+    assert str(ali) == '0.0 1.0 a\n1.0 2.0 b\n2.0 3.001 c'
+    assert str(ali[10:]) == ''
+
+
+def test_partial_read(ali):
+    a = ali[0:1]
+    assert np.array_equal(np.array([[0, 1]]), a._times)
+    assert np.array_equal(np.array(['a']), a.phones)
+
+    a = ali[0:2]
+    assert np.array_equal(np.array([[0, 1], [1, 2]]), a._times)
+    assert np.array_equal(np.array(['a', 'b']), a.phones)
+
+    a = ali[:2]
+    assert np.array_equal(np.array([[0, 1], [1, 2]]), a._times)
+    assert np.array_equal(np.array(['a', 'b']), a.phones)
+
+
+def test_complete_read(ali):
+    a = ali[0:]
+    assert np.array_equal(ali._times, a._times)
+    assert np.array_equal(ali.phones, a.phones)
+
+    a = ali[-1:]
+    assert np.array_equal(ali._times, a._times)
+    assert np.array_equal(ali.phones, a.phones)
+
+    a = ali[:5]
+    assert np.array_equal(ali._times, a._times)
+    assert np.array_equal(ali.phones, a.phones)
+
+    a = ali[-5:5]
+    assert np.array_equal(ali._times, a._times)
+    assert np.array_equal(ali.phones, a.phones)
+
+    a = ali[:]
+    assert np.array_equal(ali._times, a._times)
+    assert np.array_equal(ali.phones, a.phones)
+
+
+def test_empty_read(ali):
+    a = ali[:-1]
+    assert len(a._times) == 0
+    assert len(a.phones) == 0
+
+    a = ali[0:-1]
+    assert len(a._times) == 0
+    assert len(a.phones) == 0
+
+    a = ali[10:]
+    assert len(a._times) == 0
+    assert len(a.phones) == 0
+
+
+@pytest.mark.parametrize('t', [0, 0.5, 1, 3.001])
+def test_read_oneinstant(ali, t):
+    # read [t, t[ is like reading nothing
+    a = ali[t:t]
+    assert a.duration() == 0
+    assert len(a.phones) == 0
+
+
+def test_read_interphones(ali):
+    a = ali[0:0.8]
+    assert np.array([0, 0.8]).reshape(1, 2) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['a']))
+
+    a = ali[0:1]
+    assert np.array([0, 1]).reshape(1, 2) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['a']))
+
+    a = ali[0.2:0.8]
+    assert np.array([0.2, 0.8]).reshape(1, 2) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['a']))
+
+    a = ali[0.2:1]
+    assert np.array([0.2, 1]).reshape(1, 2) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['a']))
+
+    a = ali[1.2:1.8]
+    assert np.array([1.2, 1.8]).reshape(1, 2) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['b']))
+
+    a = ali[0.2:1.8]
+    assert np.array([[0.2, 1], [1, 1.8]]) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['a', 'b']))
+
+    a = ali[0.2:2.8]
+    assert np.array([[0.2, 1], [1, 2], [2, 2.8]]) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['a', 'b', 'c']))
+
+    a = ali[0.2:4]
+    assert np.array([[0.2, 1], [1, 2], [2, 3.001]]) == pytest.approx(a._times)
+    assert np.array_equal(a.phones, np.array(['a', 'b', 'c']))
 
 
 def test_realdata(alignments):
     ali = alignments['S01F1522_0003']
     assert ali.phones.shape == (38,)
     assert np.array_equal(ali.phones[:3], np.array(['k', 'y', 'o']))
-    assert np.array_equal(ali[:0.1425][1], np.array(['k', 'y', 'o']))
+    assert np.array_equal(ali[:0.1425].phones, np.array(['k', 'y', 'o']))
 
     assert ali.duration() == pytest.approx(3.1)
-    assert ali[3.2:][0].shape == (0, 2)
+    assert ali[3.2:].phones.shape == (0,)
 
 
 def test_sample_rate():
-    ali = Alignment([[0, 1, 'a'], [1, 2, 'b']])
-    assert list(ali.at_sample_rate(1)) == ['a', 'b']   # all
-    assert list(ali.at_sample_rate(1, 0, 1)) == ['a']  # 1st second
-    assert list(ali.at_sample_rate(4, 0, 1)) == ['a'] * 4
-    assert list(ali.at_sample_rate(4)) == ['a'] * 4 + ['b'] * 4
+    ali = Alignment.from_list([[0, 1, 'a'], [1, 3, 'b']])
+    assert list(ali.at_sample_rate(1)) == ['a', 'b', 'b']   # all
+    assert list(ali[:1].at_sample_rate(1)) == ['a']  # 1st second
+    assert list(ali[:1].at_sample_rate(4)) == ['a'] * 4
+    assert list(ali.at_sample_rate(4)) == ['a'] * 4 + ['b'] * 8
+    assert len(list(ali.at_sample_rate(100))) == ali.duration() * 100
 
+    ali = Alignment.from_list([[0, 0.8, 'a'], [0.8, 1, 'b']])
+    assert list(ali.at_sample_rate(1)) == ['a']
+    assert list(ali.at_sample_rate(2)) == ['a', 'a']
+    assert list(ali.at_sample_rate(5)) == ['a', 'a', 'a', 'a', 'b']
+    assert list(ali.at_sample_rate(10)) == ['a', 'a', 'a', 'a'] * 2 + ['b'] * 2
 
-def test_bad_init():
-    with pytest.raises(ValueError):
-        Alignment([(0, 1, 'a'), (1, 2, 'b'), (0, 3, 'a')])
+    ali = Alignment.from_list([[0, 0.2, 'a'], [0.2, 1, 'b']])
+    assert list(ali.at_sample_rate(1)) == ['a']
+    assert list(ali.at_sample_rate(2)) == ['a', 'b']
+    assert list(ali.at_sample_rate(5)) == ['a', 'b', 'b', 'b', 'b']
 
-    with pytest.raises(ValueError):
-        Alignment([(0, 1, 'a'), (0, 3, 'c')])
-
-    with pytest.raises(ValueError):
-        Alignment([(0, 0, 'a')])
-
-    with pytest.raises(ValueError):
-        Alignment([(1, 0, 'a')])
+    ali = Alignment.from_list([[0, 0.5, 'a'], [0.5, 1, 'b']])
+    assert list(ali.at_sample_rate(1)) == ['a']
+    assert list(ali.at_sample_rate(2)) == ['a', 'b']
+    assert list(ali.at_sample_rate(3)) == ['a', 'a', 'b']
 
 
 def test_load(alignments):
