e5c59cc36a1bacc45bbf56f492c85059f4e13dd0
Mathieu Bernard <mathieu_bernard@laposte.net>
2021-03-25 14:21:46 +0100

    added import shortcuts

--- shennong/postprocessor/__init__.py
+++ shennong/postprocessor/__init__.py
@@ -1,1 +1,5 @@
 """Speech features post-processors"""
+
+from shennong.postprocessor.cmvn import CmvnPostProcessor
+from shennong.postprocessor.delta import DeltaPostProcessor
+from shennong.postprocessor.vad import VadPostProcessor
